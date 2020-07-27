a5465bb5f01345378e30af19b9713301c8d696fa
alishker <alinoorshukri7@gmail.com>
2020-07-27 11:33:32 +0300

    tackled on project details and more

--- DjangoPortfolio/jobs/models.py
+++ DjangoPortfolio/jobs/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
     description = models.CharField(max_length=100)
+
+
+def __str__(self):
+    return self.description
